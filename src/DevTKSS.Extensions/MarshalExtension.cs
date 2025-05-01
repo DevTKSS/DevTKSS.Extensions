932f1d91d20169fcbf237ff62c8f556871f08138
DevTKSS <info@technische-konstruktion.com>
2025-05-01 14:23:15 +0200

    Add project files.

---
+++ src/DevTKSS.Extensions/MarshalExtension.cs
@@ -0,0 +1,43 @@
+﻿using System.Runtime.InteropServices;
+
+namespace DevTKSS.Extensions.Compartiblity;
+/// <summary>
+/// Source code: <see href="https://adndevblog.typepad.com/autocad/2024/07/autocad-2025-marshalgetactiveobject-net-core.html"/>
+/// </summary>
+internal static class MarshalExtension
+{
+    [DllImport("ole32")]
+    private static extern int CLSIDFromProgIDEx(
+        [MarshalAs(UnmanagedType.LPWStr)] string lpszProgID,
+        out Guid lpclsid);
+    [DllImport("oleaut32")]
+    private static extern int GetActiveObject(
+      [MarshalAs(UnmanagedType.LPStruct)] Guid rclsid,
+      IntPtr pvReserved,
+      [MarshalAs(UnmanagedType.IUnknown)] out object ppunk);
+
+    public static object? GetActiveObject(string progId,
+                                         bool throwOnError = false)
+    {
+        if (progId == null)
+            throw new ArgumentNullException(nameof(progId));
+
+        var hr = CLSIDFromProgIDEx(progId, out var clsid);
+        if (hr < 0)
+        {
+            if (throwOnError)
+                Marshal.ThrowExceptionForHR(hr);
+
+            return null;
+        }
+        hr = GetActiveObject(clsid, IntPtr.Zero, out var obj);
+        if (hr < 0)
+        {
+            if (throwOnError)
+                Marshal.ThrowExceptionForHR(hr);
+
+            return null;
+        }
+        return obj;
+    }
+}

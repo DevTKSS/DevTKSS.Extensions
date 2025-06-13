fea67afb188463d57b0518013da9ce2f06782880
DevTKSS <info@technische-konstruktion.com>
2025-06-13 19:29:59 +0200

    chore: apply target specifyer

--- src/DevTKSS.Extensions.Marshalling/MarshalExtensions.cs
+++ src/DevTKSS.Extensions.Marshalling/MarshalExtensions.cs
@@ -2,10 +2,10 @@ using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 // cspell:ignore rclsid ppunk lpsz lpclsid oleaut UCOMI ppbc
 namespace DevTKSS.Extensions.Marshalling;
+
 /// <summary>
 /// Source code: <see href="https://adndevblog.typepad.com/autocad/2024/07/autocad-2025-marshalgetactiveobject-net-core.html"/>
 /// </summary>
-[SupportedOSPlatform("windows")]
 public static class MarshalExtensions
 {
 
